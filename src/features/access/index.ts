31de7ca67a4f90ed29863610c9dcdc80815ee86f
rockDanAppDevelop <rockdan.israel@gmail.com>
2026-05-06 07:56:30 +0300

    Add feature access public API

---
+++ src/features/access/index.ts
@@ -0,0 +1,13 @@
+export type {
+  AccessPlan,
+  FeatureKey,
+  SubscriptionStatus,
+  UserAccess,
+} from './types/feature-access.types';
+
+export {
+  DEFAULT_USER_ACCESS,
+  FEATURE_ACCESS_MODE,
+} from './lib/feature-access.defaults';
+export { canAccessFeature } from './lib/can-access-feature';
+export { FEATURE_ACCESS_CATALOG } from './constants/feature-access.catalog';
